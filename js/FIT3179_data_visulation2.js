7e7851c49889c0d2d6fb6bc2660aafb9ecdef364
RuiGuo12345 <145431027+RuiGuo12345@users.noreply.github.com>
2023-10-17 16:42:14 +1100

    Add files via upload
    final version of dv2

---
+++ js/FIT3179_data_visulation2.js
@@ -0,0 +1,24 @@
+var vg1 = "js/G1_map.json";
+vegaEmbed('#gragh1', vg1).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg2 = "js/G2.json";
+vegaEmbed('#graph2', vg2).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg3 = "js/G3_AreaChart.json";
+vegaEmbed('#graph3', vg3).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg4 = "js/G4.json";
+vegaEmbed('#graph4', vg4).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg5 = "js/G5.json";
+vegaEmbed('#graph5', vg5).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
